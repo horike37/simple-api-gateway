0f8967506c221b71fe5afd179addcad34498b211
堀家隆宏 <horike@horiketakahiro-no-MacBook-Pro.local>
2016-07-11 00:12:52 +0900

    update unit test

---
+++ tests/all.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('All Tests', function() {
+  this.timeout(0)
+  before(function() {})
+  after(function() {})
+  
+  require('./create-spec')
+})
---
+++ tests/create-spec.js
@@ -0,0 +1,34 @@
+'use strict'
+
+var chai      = require('chai'),
+    bddStdin  = require('bdd-stdin'),
+    inquirer  = require('inquirer'),
+    aws       = require('aws-sdk'),
+    create    = require('../lib/create'),
+    should    = chai.should(),
+    apigateway
+  
+describe('API Create Tests', function() {
+  this.timeout(0)
+  before(function() {})
+  after(function() {})
+  
+  it('Create action test', function(done) {
+    let apigw = new create()
+  
+    bddStdin(bddStdin.keys.down, '\n')
+    inquirer.prompt([apigw.q.region]).then(function(answers) {
+      answers.region.should.equal('ap-northeast-1')
+      return apigw.setRegion(answers.region)
+    }).then(function() {
+      bddStdin('awsomeapi', '\n')
+      return inquirer.prompt([apigw.q.apiname])
+    }).then(function(answers) {
+      answers.name.should.equal('awsomeapi')
+      return apigw.createRestApi(answers.name)
+    }).then(function(res) {
+      process.exit()
+      done()
+    })
+  })
+})
