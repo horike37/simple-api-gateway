45f9f5baa9a259df6e7eeec5b248ed108ec8fd7a
堀家隆宏 <horike@horiketakahiro-no-MacBook-Pro.local>
2016-07-11 09:32:50 +0900

    update

---
+++ tests/config.js
@@ -0,0 +1,8 @@
+'use strict'
+
+let config = {
+  region:  'us-east-1',
+  apiname: 'test_awsomeapi'
+}
+
+module.exports = config
--- tests/create-spec.js
+++ tests/create-spec.js
@@ -1,11 +1,10 @@
 'use strict'
 
-var chai      = require('chai'),
-    bddStdin  = require('bdd-stdin'),
-    inquirer  = require('inquirer'),
-    aws       = require('aws-sdk'),
-    create    = require('../lib/create'),
-    should    = chai.should()
+var chai   = require('chai'),
+    aws    = require('aws-sdk'),
+    create = require('../lib/create'),
+    config = require('./config'),
+    should = chai.should()
   
 describe('API Create Tests', function() {
   this.timeout(0)
@@ -15,18 +14,10 @@ describe('API Create Tests', function() {
   it('Create action test', function(done) {
     let apigw = new create()
   
-    bddStdin(bddStdin.keys.down, '\n')
-    inquirer.prompt([apigw.q.region]).then(function(answers) {
-      answers.region.should.equal('ap-northeast-1')
-      return apigw.setRegion(answers.region)
-    }).then(function() {
-      bddStdin('awsomeapi', '\n')
-      return inquirer.prompt([apigw.q.apiname])
-    }).then(function(answers) {
-      answers.name.should.equal('awsomeapi')
-      return apigw.createRestApi(answers.name)
+    apigw.setRegion(config.region).then(function() {
+      return apigw.createRestApi(config.apiname)
     }).then(function(res) {
-      process.exit()
+      res.name.should.equal(config.apiname)
       done()
     })
   })
